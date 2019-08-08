0225418ac933083e8f9ab0e84727b3bbfc266c63
Yash Kapila <yash.kapila18@gmail.com>
2019-08-08 21:32:30 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,56 @@
+{
+  /*
+   * Q1.
+   * Write a program which would sort the below array in ascending order by name.
+   * Hint: String.prototype.localeCompare()
+   */
+  const people = [{
+    name: 'Richie Baros'
+  }, {
+    name: 'Jayson Karner'
+  }, {
+    name: 'Dylan Wanner'
+  }, {
+    name: 'Travis Tarwater'
+  }, {
+    name: 'Angelique Lyall'
+  }, {
+    name: 'Stacie Dyar'
+  }];
+
+  /*
+   * Q2.
+   * Write a function that accepts an array and returns only the even numbers in it.
+   * For example, if the input array is [13, 1, 4, 6, 8, 3, 2, 2], the output array should be [4, 6, 8, 2, 2].
+   */
+
+  /* 
+   * Q3.
+   * A class has 5 students defined below. Write a program to calculate average score of the class.
+   * Note: The average should be restricted to 2 decimal places.
+   */
+  const students = [{
+    name: 'Student 1',
+    score: 93
+  }, {
+    name: 'Student 2',
+    score: 87
+  }, {
+    name: 'Student 3',
+    score: 84
+  }, {
+    name: 'Student 4',
+    score: 90
+  }, {
+    name: 'Student 5',
+    score: 96
+  }];
+
+  /*
+   * Q4.
+   * Write a JavaScript function which accept a string as argument and swaps the case of each character.
+   * For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+   * Any character other than alphabets will stay as it is.
+   * Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+   */
+}
