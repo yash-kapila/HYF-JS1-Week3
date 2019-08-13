e200daa6abeb59a67f33116c5197b107bd7684bd
Abdullahkanatsiz <abdullahkanatsiz7@gmail.com>
2019-08-13 14:43:51 +0200

    added hw layout

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@
 
   /*
    * Q2.
-   * Write a function that accepts an array and returns only the even numbers in it.
+   * Write a function that accepts array and returns only the even numbers in it.
    * For example, if the input array is [13, 1, 4, 6, 8, 3, 2, 2], the output array should be [4, 6, 8, 2, 2].
    */
       let nes = [13, 1, 4, 6, 8, 3, 2, 2];
